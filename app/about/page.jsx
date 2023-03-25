55e93a941cd123d7bfc6624b7b364ac46c248361
Hamza Ashraf <hamzaashraf160@gmail.com>
2023-03-25 18:08:04 +0500

    Create The Navbar Component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div className='max-w-6xl mx-auto space-y-4'>
+    <div className='max-w-6xl mx-auto space-y-4 p-4'>
       <h1 className='text-2xl font-medium text-amber-600'>About</h1> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quibusdam quidem soluta, praesentium corporis facilis vero asperiores saepe eveniet aliquam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste totam amet, incidunt obcaecati eos itaque voluptatem eveniet dolore ut asperiores sed beatae fuga voluptate et cum eaque quo, accusamus provident quaerat reprehenderit, consectetur delectus consequatur?</p>
       <p> Aperiam ullam hic, odio eos blanditiis est optio doloremque dicta eius rem, sunt aliquid assumenda debitis quidem non! Vitae explicabo optio maxime ut impedit incidunt,</p>
