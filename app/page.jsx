897086a2711b3a31035b48cfc7752d8aac452f55
Hamza Ashraf <hamzaashraf160@gmail.com>
2023-03-25 19:08:07 +0500

    Data Fetched From IMDB

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+import Results from "./components/Results"
 
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending"
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === `fetchTopRated` ? `movie/top_rated` : `trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`, {next: { revalidate:10000 }})
+
+  if(!res.ok){
+    throw new Error(`Failed to fetch Data`)
+  }
+  const {results} = await res.json()
+  
   return (
-    <h1 className="text-blue-400 text-2xl">Hello Next Js</h1>
+    <div>
+      <Results results={results}/>
+    </div> 
   )
 }
