a23ef0753db3b082aa09dfceb5605190f3c9f187
Hamza Ashraf <hamzaashraf160@gmail.com>
2023-03-27 02:24:14 +0500

    Loader Added

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner.svg" alt="loading..." />
+    </div>
+  )
+}
+
+export default Loading
